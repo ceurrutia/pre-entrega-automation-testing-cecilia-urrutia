a64aad38d218ac1b35aae487c5464dda3ea7c19b
Cecilia Urrutia <urrutia.cici@gmail.com>
2026-04-27 12:05:32 -0300

    agrego test saucedemo con webdriver y busqueda de inputs por id

--- tests/test_saucedemo.py
+++ tests/test_saucedemo.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 def test_prueba():
     driver = webdriver.Chrome()
@@ -6,3 +7,19 @@ def test_prueba():
     assert "Swag Labs" in driver.title
     
     driver.quit()
+    
+def test_login():
+    driver = webdriver.Chrome()
+    driver.get("https://www.saucedemo.com/")
+    
+    username_input = driver.find_element(By.ID, "user-name")
+    password_input = driver.find_element(By.ID, "password")
+    login_button = driver.find_element(By.ID, "login-button")
+
+    username_input.send_keys("standard_user")
+    password_input.send_keys("secret_sauce")
+    login_button.click()
+    
+    assert "inventory" in driver.current_url
+    
+    driver.quit()   
