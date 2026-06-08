8f5dc733a75626eb87e7e9896c35790666751f86
Cecilia Urrutia <urrutia.cici@gmail.com>
2026-06-08 11:19:07 -0300

    agrego un test por cada metodo get all, post, put y delete

--- api/api_clients.py
+++ api/api_clients.py
@@ -26,12 +26,24 @@ def get_users():
         print(f"Error: {response.status_code}")
     return response
 
+##todos
 
+def get_all_users():
+    response = requests.get(f"{URL_BASE}/users?page=2", headers=HEADERS)
+    if response.status_code == 200:
+        print(response.json())
+    else:
+        print(f"Error: {response.status_code}")
+    return response
+
+
+###############post a la api###############
 def apiPost():
     response = requests.post(f"{URL_BASE}/login", headers=HEADERS, json=creds)
     success = response.json()
     print(success['token'])
 
+###############create########################
 
 def create_user(name, job):
     payload = {
@@ -41,6 +53,32 @@ def create_user(name, job):
     response = requests.post(f"{URL_BASE}/users", headers=HEADERS, json=payload)
     if response.status_code == 201:
         print(response.json())
+    else:
+        print(f"Error: {response.status_code}")
+    return response
+
+###############update####################
+
+def update_user(user_id, name, job):
+    payload = {
+        "name": name,
+        "job": job
+    }
+    response = requests.put(f"{URL_BASE}/users/{user_id}", headers=HEADERS, json=payload)
+    if response.status_code == 200:
+        print(response.json())
+    else:
+        print(f"Error: {response.status_code}")
+    return response
+
+
+
+################delete#######################
+
+def delete_user(user_id):
+    response = requests.delete(f"{URL_BASE}/users/{user_id}", headers=HEADERS)
+    if response.status_code == 204:
+        print("Usuario eliminado correctamente")
     else:
         print(f"Error: {response.status_code}")
     return response
--- tests/tests/test_all.py
+++ tests/tests/test_all.py
@@ -1,5 +1,7 @@
-from api.api_clients import create_user, create_user, get_users
+from api.api_clients import create_user, create_user, delete_user, get_all_users, get_users, update_user
 from conftest import user_data
+
+##obtiene
 
 def test_get_user():
     response = get_users()
@@ -12,8 +14,45 @@ def test_get_user():
 
 
 ##creatre
+
 def test_create_user(user_data):
     response = create_user(user_data["name"], user_data["job"])
     assert response.status_code == 201
     assert response.json()["name"] == "morpheus"
     assert response.json()["job"] == "lider de la nabucodonosor"
+    
+
+##update
+
+def test_update_user(user_data):
+    response = create_user(user_data["name"], user_data["job"])
+    assert response.status_code == 201
+    user_id = response.json()["id"]
+    
+    updated_response = update_user(user_id, "Neo", "El elegido")
+    assert updated_response.status_code == 200
+    assert updated_response.json()["name"] == "Neo"
+    assert updated_response.json()["job"] == "El elegido"
+
+    
+#deletea
+
+def test_delete_user(user_data):
+    response = create_user(user_data["name"], user_data["job"])
+    assert response.status_code == 201
+    user_id = response.json()["id"]
+    
+    delete_response = delete_user(user_id)
+    assert delete_response.status_code == 204
+    
+    
+## obtiene todos
+
+def test_get_all_users():
+    response = get_all_users()
+    if response.status_code == 200:
+        print(response.json())
+    
+    data = response.json()
+    assert "data" in data
+    assert len(data["data"]) > 0
